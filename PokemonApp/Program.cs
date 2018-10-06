e325382d212b81848146660db887f3f140ab3201
Ali Alhaddad <alialhaddadAA@outlook.com>
2018-10-06 12:29:24 -0400

    Finish Retrieving GetOnePokemon method.

---
+++ PokemonApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
